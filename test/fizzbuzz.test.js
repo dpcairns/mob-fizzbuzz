363282f4eabbb46ed2f5259b14087b17b8216f2b
harriet-the-spy <dpcairns@gmail.com>
2020-06-26 11:01:30 -0700

    respond to hannah feedback

--- test/fizzbuzz.test.js
+++ test/fizzbuzz.test.js
@@ -33,3 +33,19 @@ test('it should take in 10 and return the appropriate fizzbuzz as a string', (ex
     expect.equal(actual, expected);
 });
 
+
+test('it should take in 7 and return the appropriate fizzbuzz as a string', (expect) => {
+    //Arrange
+    // Set up your arguments and expectation
+    const input = 7;
+    const expected = ' 1 2 Fizz 4 Buzz Fizz 7';
+    
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = makeFizzBuzz(input);
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+
