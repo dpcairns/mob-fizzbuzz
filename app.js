fdaf1f5de916d12f9eefa384920810d824f98354
Dakota Druley <58957027+dakotadruley@users.noreply.github.com>
2020-06-26 11:49:05 -0700

    created resultSpan which updates per user input

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 // import functions and grab DOM elements
+import { makeFizzBuzz } from './utils.js';
 
 // initialize state
 
 const input = document.getElementById('input');
 const button = document.getElementById('button');
+
 // set event listeners to update state and DOM
 
 button.addEventListener('click', () => {
   console.log(input.value);
+
+  const resultSpan = document.getElementById('output');
+  const inputValue = Number(input.value);
+
+  resultSpan.textContent = makeFizzBuzz(inputValue);
+
 });
+
